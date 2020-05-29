d821c9e91e6f2aff56e0ae79ea509e6d24ef365a
Andrey Kondakov <wenom86@mail.ru>
2020-05-29 21:25:53 +0400

    1
    перенос кода из AutoWarmFloor

--- AutoWatering_v1/AutoWatering_v1.ino
+++ AutoWatering_v1/AutoWatering_v1.ino
@@ -1,33 +1,57 @@
 //=============================================================
 // НАЗВАНИЕ ПРОГРАММЫ: AutoWatering
-// ВЕРСИЯ ПРОГРАММЫ: 0.1 (01.04.2020г)
+// ВЕРСИЯ ПРОГРАММЫ: 0.1 (29.05.2020г)
 // ВЕРСИЯ ARDUINO IDE: 1.8.11
 //=============================================================
+#include <AmperkaKB.h>          // + библ. работы с клавиатурой 1х4,4х3,4х4.
 #include <OneWire.h>            // + для термодатчика 18b20
 #include <LiquidCrystal.h>      // + библ. LCD-экрана.
 #include <DS3231.h>             // + библ. работы с модулем часов.
-#include <AmperkaKB.h>          // + библ. работы с клавиатурой 1х4,4х3,4х4.
-float version = 0.1;            // ВЕРСИЯ ПРОГРАММЫ
+float version = 0.2;            // ВЕРСИЯ ПРОГРАММЫ
 
+AmperkaKB KB(42, 43, 44, 45, 46, 47, 48, 49); //номера пинов ардуино, подключенные к шлейфу клавиатуры (нумерация слева-направо)
 OneWire ds(7);                  // линия 1-Wire (DS18b20 и т.д) будет на pin 7
 LiquidCrystal lcd(22, 28, 23, 24, 25, 26, 27);   // для МЕГИ 2560. (A=5v/K=GND)(VSS=GND/VDD=5v/RS=pin22/RW=pin28/E=pin23/D4=pin24/D5=pin25/D6=pin26/D7=pin27).
 DS3231  rtc(20, 21);            //шина, на которую подключены часы (для ардуино нано на 328 чипе, это пины A4/A5)(для меги - пины 20/21).
+long previousMillisLCDbrightness = 0; //счетчик прошедшего времени для Автовылючения подсветки LCD
+unsigned long IntervalReadSensorDS = 5000;                // интервал считывания показаний с DS18b20.
+unsigned long ValueMillisPreviousReadSensorDS = 0;
+long intervalLCDbrightness = 30000;  //задержка Автовыключения подсветки LCD (по умолчанию 300000 = 5 минут)
 #define LCDbright 31            // управление вкл/выкл подсветки экрана на 31 пине. 
+int T1,T2,t3,t4;                // инициализируем переменные для значений температуры..... (T1-подача в теплый пол, T2-обратка из теплого пола, t3-желаемая темп. в помещении, t4-фактическая темп. в помещении)
+int ekran;                      //переменная для выбора экрана меню.
+int m=0;                        //переменная для экранов меню.
 
 
 void setup() {
-
-lcd.begin(20, 4);    // инициализация экрана (20 символов в 4 строках).
-delay(500);          // ожидание
-rtc.begin();         // инициализация часов на DS3231.
-delay(500);          // ожидание
+KB.begin(KB4x4, 3000); // инициализация клавиатуры. значение 3000 - время длительного нажатия кнопки.  
+delay(500);            // ожидание
+Serial.begin(9600);    // инициализация сериал-порта 0, для вывода служебной инф-ии в монитор порта.
+delay(500);            // ожидание  
+lcd.begin(20, 4);      // инициализация экрана (20 символов в 4 строках).
+delay(500);            // ожидание
+rtc.begin();           // инициализация часов на DS3231.
+delay(500);            // ожидание
 pinMode(LCDbright, OUTPUT);           //выводы устанавливаюся в режим выхода
 digitalWrite(LCDbright, HIGH);        //на выходе высокий уровень
-privetstvie();      // Выводим приветствие на экран при включении питания(название, версия, часы и т.д.) (вкладка Read_LCD)
+privetstvie();         // Выводим приветствие на экран при включении питания(название, версия, часы и т.д.) (вкладка Read_LCD)
 
 
 }
 void loop() {
 
+unsigned long currentMillis = millis();                                                                //Присваивание значения общей переменной отсчета времени.                                                                
+
+if(currentMillis - previousMillisLCDbrightness > intervalLCDbrightness)                                //Если счетчик достиг интервала. 
+{previousMillisLCDbrightness = currentMillis;  digitalWrite(LCDbright, LOW); Serial.println("LCDbright OFF");}  //выключаем подсветку LCD, сообщаем в serial.
+if(currentMillis - ValueMillisPreviousReadSensorDS > IntervalReadSensorDS)                             //Если счетчик превысил интервал, то:
+{ ds18(); ValueMillisPreviousReadSensorDS = currentMillis; Serial.print("18b20: "); Serial.println(ds18()); }  //Cчитываем показания DS18b20, сообщаем в serial.
+
+
+
+NajatieUpDownLeftRight();    // Обрабатываем нажатие клавиш Вверх/Вниз. (вкладка Read_KEYBOARD)
+Ekran();                     // Отображение экрана в зависимости от переменной m (вкладка Read_LCD)
+Read_ekran();                // Отображаем содержимое выбранного экрана (вкладка Read_LCD)
+                                                       
 
 }
--- AutoWatering_v1/Read_LCD.ino
+++ AutoWatering_v1/Read_LCD.ino
@@ -3,20 +3,97 @@
 void privetstvie() 
 {     
    lcd.setCursor(0, 0);
-   lcd.print("----AutoWatering----");    // ---AutoWatering----
+   lcd.print("---AutoWarmFloor---");    // ---AutoWatering----
    lcd.setCursor(0, 1);                  // *DATE: 08.08.1976*
    lcd.print("DATE: ");                  // *TIME: 22:08:54  *
    lcd.print(rtc.getDateStr()); 
    lcd.setCursor(0, 2);         
    lcd.print("TIME: ");         
    lcd.print(rtc.getTimeStr());
+   lcd.setCursor(0, 3);  
+   lcd.print("T1:"); lcd.setCursor(5, 0); lcd.print(ds18());
    lcd.setCursor(16, 3);         
    lcd.print("v");
    lcd.setCursor(17, 3); 
    lcd.print(version, 1);
-   delay (5000);
+   delay (3000);
    lcd.clear();
    
    
 } //                   КОНЕЦ ВЫВОДА ПРИВЕТСТВИЯ НА ЭКРАН ПРИ ВКЛЮЧЕНИИ
+//=================================================================================================
+
+//=================================================================================================
+//                   НАЧАЛО ОБРАБОТКИ ВЫБОРА ОТОБРАЖАЕМОГО ЭКРАНА В ЗАВИСИМОСТИ ОТ ПЕРЕМЕННОЙ "m"   
+void Ekran()
+   {
+   if (m==0){ ekran = 0; }
+   if (m==1){ ekran = 10; }
+   if (m==2){ ekran = 2; }
+   if (m==3){ ekran = 3; }
+   if (m==4){ ekran = 4; }
+   if (m==5){ ekran = 5; }
+   if (m==6){ ekran = 6; }
+   if (m==7){ ekran = 7; }
+   if (m==8){ ekran = 8; }
+   if (m==9){ ekran = 9; }
+   if (m==10){ ekran = 10; }
+   if (m==11){ ekran = 11; }
+   if (m==12){ ekran = 12; }
+   if (m==13){ ekran = 13; }
+   
+} //                   КОНЕЦ ОБРАБОТКИ ВЫБОРА ОТОБРАЖАЕМОГО ЭКРАНА В ЗАВИСИМОСТИ ОТ ПЕРЕМЕННОЙ "m"
+//=================================================================================================
+
+//=================================================================================================
+//                   НАЧАЛО ОТОБРАЖЕНИЯ ИНФОРМАЦИИ НА ЭКРАНАХ      
+void Read_ekran(){
+
+if (ekran==0){                 //переменная ekran=0 (+), главный экран.                               
+   lcd.setCursor(0, 0);  lcd.print("T1:"); lcd.setCursor(5, 0); lcd.print(ds18()); lcd.setCursor(8, 0); lcd.print("t3:"); lcd.setCursor(12, 0); lcd.print(ds18());
+   lcd.setCursor(0, 1);  lcd.print("T2:"); lcd.setCursor(5, 1); lcd.print(ds18()); lcd.setCursor(8, 1); lcd.print("t4:"); lcd.setCursor(12, 1); lcd.print(ds18());
+   lcd.setCursor(12, 2); lcd.print(rtc.getTimeStr());
+   lcd.setCursor(10, 3); lcd.print(rtc.getDateStr()); 
+   } 
+  
+else if (ekran==10){  
+   }
+
+else if (ekran==2){
+   }
+   
+else if (ekran==3){  
+   }
+
+else if (ekran==4){ 
+   }   
+
+else if (ekran==5){  
+   }
+   
+else if (ekran==6){  
+   }
+   
+else if (ekran==7){ 
+   }   
+
+else if (ekran==8){  
+   }
+
+else if (ekran==9){ 
+   }
+
+else if (ekran==10){ 
+   }
+   
+else if (ekran==11){
+    }
+
+else if (ekran==12){ 
+    }
+   
+else if (ekran==13){
+    }   
+   
+} //                  КОНЕЦ ОТОБРАЖЕНИЯ ИНФОРМАЦИИ НА ЭКРАНАХ  
 //=================================================================================================
